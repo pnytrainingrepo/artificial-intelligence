7f88a123d306f2730f5c8bfc18c0d1faec0272c0
Azam Rashid <azamrashid@gmail.com>
2024-07-18 18:58:15 +0500

    ok

--- python-codes/hello-world/hello.py
+++ python-codes/hello-world/hello.py
@@ -1,1 +1,39 @@
-print("Hello World");
+# print("Hello Pny Training")
+# print(2+2)
+
+
+""" myVariable = "PNY"
+print(myVariable)
+
+
+
+x ="awesome"
+def myfunc():
+        x="Freelance"
+        print("Python is "+ x)
+    
+myfunc()
+
+y = int(2.9)
+print(y)
+print(type(y))
+
+print(10>9)
+print("Time is Money")
+
+#print("Time "of" Money")
+
+print('Time "is" Money')
+
+"""
+
+
+word = "  Hello World  "
+print(word[:5])
+print(word.upper())
+print(word.strip())
+print(word.split())
+
+age=30
+text=f"I go to university in {age:.1f} Age"
+print(text)
